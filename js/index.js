99867996eaee981dd9300e172d0157f3d1d96aac
Leandro F <leandroferreirads722@gmail.com>
2026-03-31 17:42:11 -0300

    feat: adiciona display none

--- js/index.js
+++ js/index.js
@@ -4,13 +4,18 @@ const characterPreview = document.querySelector(".character-preview img")
 
 characterSelected.forEach(img => {
     img.addEventListener("click", async (event) => {
-        
+        try{
         const characterSelected = event.target.dataset.name;
         const response = await fetch(`${baseURL}/characters?name=${characterSelected}`);
         
         const data = await response.json();
+
+        characterPreview.style.display = "block"
         
         characterPreview.setAttribute("src", `https://dragonball-api.com/characters/${characterSelected}.webp`)
+        }catch(error) {
+            console.log("Não foi possível encontrar a imagem do personagem, erro:", error)
+        }
     })
 });
 
